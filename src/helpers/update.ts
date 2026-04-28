553abbd0edd1399d7bd6eae8dc428b58845c640a
ADAM Mario <adam.mario@icloud.com>
2026-04-28 11:34:30 +0200

    update function

--- src/helpers/update.ts
+++ src/helpers/update.ts
@@ -2,5 +2,5 @@ import util from 'util';
 import {execSync} from 'child_process';
 
 export async function update() {
-  return execSync('curl -L -O https://github.com/Mario-35/trace/raw/refs/heads/main/dist.zip && unzip -qq -o $FILEDIST -d ./trace/').toString();
+  return execSync('curl -L -O https://github.com/Mario-35/trace/raw/refs/heads/main/dist.zip && unzip -qq -o dist.zip -d ./trace/').toString();
 }
